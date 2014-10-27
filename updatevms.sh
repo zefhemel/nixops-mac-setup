c7e38a93d9efa9569c5fe9f759e8bdeb6ea9fdcb
Rene Donner <code@donner.at>
2014-10-27 10:07:19 +0100

    simplify updatevms.sh

--- updatevms.sh
+++ updatevms.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-nixops ssh-for-each "if test -z \"`nix-env --version | grep '(Nix) 1.7'`\"; then nix-install-package --non-interactive --url `curl -sL http://hydra.nixos.org/job/nix/master/build.x86_64-linux/latest | grep .nixpkg | grep href | grep -o \".*\" | sed s/\"//g`; fi"
+URL=`curl -sL http://hydra.nixos.org/job/nix/master/build.x86_64-linux/latest | grep .nixpkg | grep href | grep -o \".*\" | sed s/\"//g`
+nixops ssh-for-each "if test -z \"`nix-env --version | grep '(Nix) 1.7'`\"; then nix-install-package --non-interactive --url $URL ; fi"
