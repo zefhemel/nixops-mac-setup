b72a8158f7c3eebc3cf2db9d47bd270144c4c52a
Rene Donner <code@donner.at>
2014-10-27 09:11:11 +0100

    use latest nix binary directly, dont change channel settings

--- updatevms.sh
+++ updatevms.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-nixops ssh-for-each "if test -z \"`nix-env --version | grep 1.7`\"; then nix-channel --add https://nixos.org/channels/nixos-unstable && nix-channel --update && nix-env -i nix; fi"
+nixops ssh-for-each "if test -z \"`nix-env --version | grep '(Nix) 1.7'`\"; then nix-install-package --non-interactive --url `curl -sL http://hydra.nixos.org/job/nix/master/build.x86_64-linux/latest | grep .nixpkg | grep href | grep -o \".*\" | sed s/\"//g`; fi"
