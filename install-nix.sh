6e87e4f2af11c4c43c2aa03987d23da8afe927e6
Alexandre Andrade <alexandre.andrade@ecetera.com.au>
2015-05-15 21:55:28 +1000

    Nix updated to 1.8

--- install-nix.sh
+++ install-nix.sh
@@ -4,14 +4,16 @@ DIST="`pwd`"
 DMG=~/NixStore.dmg
 
 echo "Creating disk image"
-hdiutil create -size 20G -fs "Case-sensitive HFS+" -volname NixStore $DMG
+hdiutil create -size 8G -fs "Case-sensitive HFS+" -volname NixStore $DMG
 sudo mkdir -p /nix
 sudo chown $USER /nix
 ./attach-disk.sh
 
 echo "Downloading and installing Nix"
 cd /
-curl http://hydra.nixos.org/build/6039370/download/1/nix-1.6-x86_64-darwin.tar.bz2 | sudo tar xjf -
+#curl http://hydra.nixos.org/build/6039370/download/1/nix-1.6-x86_64-darwin.tar.bz2 | sudo tar xjf -
+curl http://hydra.nixos.org/build/17897581/download/1/nix-1.8-x86_64-darwin.tar.bz2 | sudo tar xjf -
+./nix-1.8-x86_64-darwin/install
 sudo chown -R $USER /nix
 rm -f /usr/local/etc/profile.d/nix.sh
 nix-finish-install
